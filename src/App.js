b50320f5148099f0ba5495d6277d3a26d99116a1
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-05-27 19:22:03 +0100

    add axios and async/await

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AstClass from './components/AstClass.js'
 import AstFunc from './components/AstFunc.js'
+import AxAsAw from './components/AxAsAw.js'
 import './App.css';
 
 function App () {
@@ -9,9 +10,13 @@ function App () {
         <AstClass/>
         <br/>
         <AstFunc/>
+        <br/>
+        <AxAsAw/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// https://illusionalcoder.medium.com/fetch-and-display-data-on-react-frontend-4ab9bccd00b6
